d86d848e92ad05cf2a85ca531c33bf37ed9d0113
kradme <coolang@yeah.net>
2018-06-22 11:52:32 +0800

    创建项目

---
+++ src/main/java/cn/lang/server/entity/Cat.java
@@ -0,0 +1,44 @@
+package cn.lang.server.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cat{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	@Column(name="AGE")
+	private int age;
+	@Column(name="NAME")
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
