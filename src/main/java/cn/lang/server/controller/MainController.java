e0dcc2deab35b98492b7e71d697be4f72ca462fb
langxp <coolang@yeah.net>
2018-06-27 14:42:53 +0800

    spring security

--- src/main/java/cn/lang/server/controller/MainController.java
+++ src/main/java/cn/lang/server/controller/MainController.java
@@ -1,45 +1,35 @@
 package cn.lang.server.controller;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import cn.lang.server.entity.Cat;
-import cn.lang.server.repo.ICatRepoDAO;
-
-@RestController
-@RequestMapping("/")
+@Controller
 public class MainController {
-	
-	@Autowired
-	private ICatRepoDAO iCat;
+	@RequestMapping("/")
+	public String root() {
+		return "redirect:/index";
+	}
 
-	@GetMapping("getCat")
-	public Cat test(int id) {
-		return iCat.findACat(id);
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
 	}
-	@PostMapping("updateCat")
-	public void test(int id, String name) {
-		iCat.updateCat(id, name);
+
+	@RequestMapping("/user/index")
+	public String userIndex() {
+		return "user/index";
 	}
-	@PostMapping("insertCat")
-	public Cat insertCat(String name, int age) {
-		return iCat.save(new Cat(name,age));
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
 	}
-	@GetMapping("findAllCats")
-	public List<Cat> findAllCats(){
-		List<Cat> list = new ArrayList<Cat>();
-		Iterable<Cat> iterable = iCat.findAll();
-		Iterator<Cat> iterator = iterable.iterator();
-		while(iterator.hasNext()) {
-			list.add(iterator.next());
-		}
-		return list;
+
+	@RequestMapping("/login-error")
+	public String loginError(Model model) {
+		model.addAttribute("loginError", true);
+		return "login";
 	}
+	
 }
