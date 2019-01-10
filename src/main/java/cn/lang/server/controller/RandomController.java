03d0424428099b6c5aac97554f6e21eea66458d6
langxp <coolang@yeah.net>
2019-01-10 13:52:52 +0800

    添加入参

--- src/main/java/cn/lang/server/controller/RandomController.java
+++ src/main/java/cn/lang/server/controller/RandomController.java
@@ -1,17 +1,18 @@
 package cn.lang.server.controller;
 
 import cn.lang.server.utils.RandomUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-@RequestMapping("/")
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class RandomController {
 
-    @GetMapping("/random")
-    public String randomPwd(@RequestParam("length") Integer length){
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/random")
+    public String randomPwd(Integer length){
+        logger.debug("收到请求，length={}", length);
         if (length==null){
             return RandomUtils.randomwPwd(8);
         }else {
--- src/main/java/cn/lang/server/utils/RandomUtils.java
+++ src/main/java/cn/lang/server/utils/RandomUtils.java
@@ -1,8 +1,12 @@
 package cn.lang.server.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Random;
 
 public class RandomUtils {
+    static Logger logger = LoggerFactory.getLogger(RandomUtils.class);
     static char[] chars = new char[52];
     static {
         for (int i=0;i<26;i++){
@@ -18,7 +22,9 @@ public class RandomUtils {
             int ranInt = new Random().nextInt(52);
             stringBuffer.append(chars[ranInt]);
         }
-        return stringBuffer.toString();
+        String pwd = stringBuffer.toString();
+        logger.info("随机密码={}", pwd);
+        return pwd;
     }
 
     public static void main(String[] args) {
