b583b2fe05ca593f6357fbd7b3970e27a7b8aad5
langxp <coolang@yeah.net>
2019-01-10 18:51:14 +0800

    添加分页

--- src/main/java/cn/lang/server/controller/WebController.java
+++ src/main/java/cn/lang/server/controller/WebController.java
@@ -6,12 +6,18 @@ import cn.lang.server.utils.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 import java.util.List;
@@ -24,10 +30,15 @@ public class WebController {
     private PwdLogRepo pwdLogRepo;
 
     @GetMapping(value = "/random")
-    public String list(Model model){
-        List<PwdLog> pwdLogList = pwdLogRepo.findAllList();
-        model.addAttribute("pwdlogs", pwdLogList);
-        return "index";
+    public ModelAndView list(Model model, Integer start, Integer limit){
+        start = (start==null||start<0) ? 0 :start;
+        limit = (limit==null||limit<=0) ? 20 :limit;
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, limit, sort);
+        Page<PwdLog> all = pwdLogRepo.findAll(pageable);
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("page", all);
+        return modelAndView;
     }
 
     @PostMapping("/genRan")
--- src/main/java/cn/lang/server/repo/PwdLogRepo.java
+++ src/main/java/cn/lang/server/repo/PwdLogRepo.java
@@ -1,12 +1,13 @@
 package cn.lang.server.repo;
 
 import cn.lang.server.entity.PwdLog;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PwdLogRepo extends CrudRepository<PwdLog, Integer> {
+public interface PwdLogRepo extends JpaRepository<PwdLog, Integer> {
 
     @Query("select p from pwd_log p order by  p.id desc")
     List<PwdLog> findAllList();
